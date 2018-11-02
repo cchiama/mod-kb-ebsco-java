80c2f4c309ca6a292f1830a63a42c13d73360aa2
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2018-11-02 09:39:28 +0200

    MODKBEKBJ-19 - Implement GET /eholdings/providers/{provider_id}
    * Implemented get providers by id method.
    * Mapped vendor returned from RM API to a single Provider
    * Added permissions to the request

--- src/main/java/org/folio/rest/converter/VendorConverter.java
+++ src/main/java/org/folio/rest/converter/VendorConverter.java
@@ -1,15 +1,9 @@
 package org.folio.rest.converter;
 
-import org.folio.rest.jaxrs.model.MetaDataIncluded;
-import org.folio.rest.jaxrs.model.MetaTotalResults;
-import org.folio.rest.jaxrs.model.Packages;
-import org.folio.rest.jaxrs.model.ProviderCollection;
-import org.folio.rest.jaxrs.model.ProviderListDataAttributes;
-import org.folio.rest.jaxrs.model.Providers;
-import org.folio.rest.jaxrs.model.Relationships;
-import org.folio.rest.jaxrs.model.Token;
+import org.folio.rest.jaxrs.model.*;
 import org.folio.rest.util.RestConstants;
 import org.folio.rmapi.model.Vendor;
+import org.folio.rmapi.model.VendorById;
 import org.folio.rmapi.model.Vendors;
 
 import java.util.List;
@@ -18,9 +12,11 @@ import java.util.stream.Collectors;
 public class VendorConverter {
 
   private static final Relationships EMPTY_PACKAGE_RELATIONSHIP = new Relationships()
-    .withPackages(new Packages().withMeta(
-      new MetaDataIncluded()
-            .withIncluded(false)));
+    .withPackages(new Packages()
+      .withMeta(new MetaDataIncluded()
+        .withIncluded(false))
+      .withData(null));
+  private final String PROVIDERS_TYPE = "providers";
 
   public ProviderCollection convert(Vendors vendors) {
     List<Providers> providerList = vendors.getVendorList().stream()
@@ -36,7 +32,7 @@ public class VendorConverter {
     String token = vendor.getVendorToken();
     return new Providers()
       .withId(String.valueOf(vendor.getVendorId()))
-      .withType("providers")
+      .withType(PROVIDERS_TYPE)
       .withAttributes(new ProviderListDataAttributes()
         .withName(vendor.getVendorName())
         .withPackagesTotal(vendor.getPackagesTotal())
@@ -44,5 +40,26 @@ public class VendorConverter {
         .withSupportsCustomPackages(vendor.isCustomer())
         .withProviderToken(token != null ? new Token().withValue(token) : null))
       .withRelationships(EMPTY_PACKAGE_RELATIONSHIP);
+  }
+
+  public Provider convertToVendor(VendorById vendor) {
+    String token = vendor.getVendorToken();
+    return new Provider()
+      .withData(new ProviderData()
+        .withId(String.valueOf(vendor.getVendorId()))
+        .withType(PROVIDERS_TYPE)
+        .withAttributes(new ProviderDataAttributes()
+          .withName(vendor.getVendorName())
+          .withPackagesTotal(vendor.getPackagesTotal())
+          .withPackagesSelected(vendor.getPackagesSelected())
+          .withSupportsCustomPackages(vendor.isCustomer())
+          .withProviderToken(token != null ? new Token().withValue(token) : null)
+          .withProxy(new Proxy()
+            .withId(vendor.getProxy().getId())
+            .withInherited(vendor.getProxy().getInherited()))
+        )
+        .withRelationships(EMPTY_PACKAGE_RELATIONSHIP))
+      .withIncluded(null)
+      .withJsonapi(RestConstants.JSONAPI);
   }
 }
